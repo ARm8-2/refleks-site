3519e6619c84a4124502a489207eb14ea38f569e
ARm8-2 <mattis.vanderdonckt@gmail.com>
2026-04-29 20:02:17 +0000

    Initial site implementation

---
+++ astro.config.mjs
@@ -0,0 +1,19 @@
+// @ts-check
+import sitemap from '@astrojs/sitemap';
+import { defineConfig } from 'astro/config';
+
+import tailwindcss from '@tailwindcss/vite';
+
+// https://astro.build/config
+export default defineConfig({
+  site: 'https://refleksapp.com',
+  outDir: './docs',
+  redirects: {
+    '/features': '/',
+    '/docs/sessions': '/docs/history'
+  },
+  integrations: [sitemap()],
+  vite: {
+    plugins: [tailwindcss()]
+  }
+});
